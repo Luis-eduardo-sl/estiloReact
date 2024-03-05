d803cda875e166ea25c6800550acb7eefd5bec6d
Luis-eduardo-sl <luis.lucena@aluno.ifsp.edu.br>
2024-03-05 17:46:46 -0300

    new card

--- src/App.js
+++ src/App.js
@@ -1,14 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
+import Paragrafo from './components/Paragrafo';
+import Card from './components/Card.js';
+import Content from './components/Content.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        
+        <Paragrafo 
+          Texto= "Luis dbdvte fgbhreybdv vevbteb"
+          Titulo= "Meu titulo"
+        />
+        <Card>
+          <Content 
+            Img = "https://avatars.githubusercontent.com/u/133153563?v=4"
+            Titulo ="Luis"
+            Text= "A descrição é a enumeração das características próprias dos seres, coisas, cenários, ambientes, costumes, impressões etc. A visão, o tato, a audição, o olfato e o paladar constituem a base da descrição"/>
+       
+
+          <Content 
+            Img = "https://avatars.githubusercontent.com/u/133153563?v=4"
+            Titulo ="Luis"
+            Text= "A descrição é a enumeração das características próprias dos seres, coisas, cenários, ambientes, costumes, impressões etc. A visão, o tato, a audição, o olfato e o paladar constituem a base da descrição"/>
+      
+
+          <Content 
+            Img = "https://avatars.githubusercontent.com/u/133153563?v=4"
+            Titulo ="Luis"
+            Text= "A descrição é a enumeração das características próprias dos seres, coisas, cenários, ambientes, costumes, impressões etc. A visão, o tato, a audição, o olfato e o paladar constituem a base da descrição"/>
+     
+
+          <Content 
+            Img = "https://avatars.githubusercontent.com/u/133153563?v=4"
+            Titulo ="Luis"
+            Text= "A descrição é a enumeração das características próprias dos seres, coisas, cenários, ambientes, costumes, impressões etc. A visão, o tato, a audição, o olfato e o paladar constituem a base da descrição"/>
+       
+
+          <Content 
+            Img = "https://avatars.githubusercontent.com/u/133153563?v=4"
+            Titulo ="Luis"
+            Text= "A descrição é a enumeração das características próprias dos seres, coisas, cenários, ambientes, costumes, impressões etc. A visão, o tato, a audição, o olfato e o paladar constituem a base da descrição"/>
+
+          <Content 
+            Img = "https://avatars.githubusercontent.com/u/133153563?v=4"
+            Titulo ="Luis"
+            Text= "A descrição é a enumeração das características próprias dos seres, coisas, cenários, ambientes, costumes, impressões etc. A visão, o tato, a audição, o olfato e o paladar constituem a base da descrição"/>
+        </Card> 
+
+        
+        
         <a
           className="App-link"
           href="https://reactjs.org"
